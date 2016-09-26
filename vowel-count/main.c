304fd9433d11b0c08e873ec4ddfee8de849db012
swh <shih777577@gmail.com>
2016-09-27 00:33:16 +0800

    Finished assignment 1

---
+++ vowel-count/main.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  // A string is represented as a character array.
+  // A word of string is of maximum 30 characters.
+  char word[31];
+  int word_count = 0; // Word count; initial value is 0.
+  int vowel_count = 0; // Vowel count; initial value is 0.
+  int i, j; // Loop variable.
+
+  while (scanf("%s", word)==1) {
+    for (j=0; j<strlen(word); j++) {
+      switch (word[j]) {
+        case '-':
+            if(word[j+1] == '-') goto Skip;
+        case '\'':
+            word[j] = ' ';
+        case '\"':
+            word[j] = ' ';
+        case '!':
+            word[j] = ' ';
+        case ',':
+            word[j] = ' ';
+        case ';':
+            word[j] = ' ';
+        case '.':
+            word[j] = ' ';
+        // case '\n':
+        //     word[j] = ' ';
+      }
+
+      if (word[j] != ' ') {
+        printf("%c", word[j]);
+      }
+    }
+    Print:
+
+    printf("#");
+
+
+    word_count += 1;
+    if (word_count==12) {
+      printf("\n");
+      word_count = 0;
+    }
+
+    Skip:
+
+    for (i=0; i<strlen(word); i++) // Check every character is word.
+    // If it is a vowel.
+    if (word[i]=='A' || word[i]=='E' || word[i]=='I' || word[i]=='O' ||word[i]=='U' ||
+    word[i]=='a' || word[i]=='e' || word[i]=='i' || word[i]=='o' || word[i]=='u')
+    vowel_count = vowel_count + 1; // Increment the vowel count.
+
+  }
+
+  if (word_count!=0) printf("\n"); // The current line is not finished. Output a newline.
+  // Print a newline before the line contents. That is, print an empty line.
+  printf("\nTotal %d vowels.\n", vowel_count); // Output the total number of vowels.
+
+  return 0;
+}
