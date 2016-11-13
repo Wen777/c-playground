087a7b2cee4e4ed40082836754e4281470c8c465
swh <shih777577@gmail.com>
2016-11-13 20:49:55 +0800

    HOMEWORK #5 done

---
+++ hexadecimal_operation/assignment_5_1_D0565617.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<math.h>
+#include<ctype.h>
+
+// for loop scanf
+// if both input are 0, break
+// hexamal to decimal
+// condition  decimal - F000 0000 0000 0000 > 0, cary flag
+// false: printf("%llX")
+
+void printChar(int loop, char character) {
+    int i = 0;
+    for(; i < loop; i++) {
+        printf("%c", character);
+    }
+}
+int main(void) {
+    unsigned long long input_1 = 0, input_2 = 0;
+    while(1) {
+        scanf("%llX %llX", &input_1, &input_2);
+        if (input_1 == 0 && input_2 == 0) {
+            break;
+        }
+        
+        printf("  %16llX\n", input_1);
+        printf("+ %16llX\n", input_2);
+        printChar(18, '-');
+        printf("\n");
+        printf("  %16llX\n", input_1 + input_2);
+        if(!((int)(input_1  - 0xFFFFFFFFFFFFFFFF +  input_2) >= 0 )) {
+            printf("%llu + %llu = %llu ****Overflow!!!\n\n", input_1 , input_2, input_1 + input_2);
+
+        } else {
+            printf("%llu + %llu = %llu\n\n", input_1 , input_2, input_1 + input_2);
+        }
+
+    }
+    return 0;
+}
