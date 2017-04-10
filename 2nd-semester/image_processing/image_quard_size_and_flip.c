f64094fddb64dae537dcbeea5006f11b68c160e9
swh <swh@hsiang.io>
2017-04-10 21:52:10 +0800

    assignment 15 almost done

--- 2nd-semester/image_processing/image_quard_size_and_flip.c
+++ 2nd-semester/image_processing/image_quard_size_and_flip.c
@@ -25,6 +25,124 @@ typedef struct
   unsigned ImportantColors;   // Number of colors that are considered important when rendering the image.
 } Header;
 
+void shrink(unsigned char *targetImageData, unsigned char *sourceImageData, Header header)
+{
+  int rowSize;  // Number of bytes in a row.
+  int fillings; // Number of bytes of padding 0's.
+  int i, j, k;  // Loop variable.
+
+  int smallRowSize;
+  int smallFillings;
+  int m;
+  int smallWidth, smallHeight, smallImageSize;
+
+  // Shrink
+  smallWidth = header.Width / 2;
+  smallHeight = header.Height / 2;
+  smallFillings = (4 - (smallWidth * 3) % 4) % 4;
+  smallRowSize = smallWidth * 3 + smallFillings;
+  smallImageSize = smallHeight * smallRowSize;
+
+  fillings = (4 - (header.Width * 3) % 4) % 4; // The number of filling bytes at the end of a row.
+  rowSize = header.Width * 3 + fillings;       // The number of bytes in a row.
+
+  //
+  // Shrink the size of original image and store it into new image.
+  //
+  for (i = 0; i < header.Height; i += 2)
+  { // Go through all rows.
+    for (j = 0; j < header.Width; j += 2)
+    {                          // Go through all pixels in row i.
+      k = i * rowSize + j * 3; // The starting index in imageData of the pixel to be transformed.
+      m = (i / 2) * smallRowSize + (j / 2) * 3;
+
+      targetImageData[m] = sourceImageData[k];         // value of B
+      targetImageData[m + 1] = sourceImageData[k + 1]; // Copy to value of G.
+      targetImageData[m + 2] = sourceImageData[k + 2]; // Copy to value of R.
+    }
+  }
+}
+
+void mirror(unsigned char *targetImageData, Header header)
+{
+  int rowSize;  // Number of bytes in a row.
+  int fillings; // Number of bytes of padding 0's.
+  int i, j, k;  // Loop variable.
+
+  // right and left
+  int k_left;        // Index of the left-half pixel.
+  int k_right;       // Index of the right-half pixel.
+  unsigned int temp; // Temporary memory for pixel swapping.
+
+  fillings = (4 - (header.Width * 3) % 4) % 4; // The number of filling bytes at the end of a row.
+  rowSize = header.Width * 3 + fillings;       // The number of bytes in a row.
+
+  //
+  // Turn the image to right.
+  //
+  for (i = 0; i < header.Height; i++)
+  { // Go through all rows.
+    for (j = 0; j < header.Width / 2; j++)
+    {                                                     // Split the image in to the left half and the right half.
+      k_left = i * rowSize + j * 3;                       // The index of the swapped pixel on the left hlaf in row i.
+      k_right = i * rowSize + (header.Width - 1 - j) * 3; // The index of the swapped pixel on the right hlaf in row i.
+      // Swap the j-th pixel and (Width-1-j)-th pixel in the i-th row.
+      // Swap blue level.
+      temp = targetImageData[k_left];                     // Store the left value to temporary memory.
+      targetImageData[k_left] = targetImageData[k_right]; // Copy the right value to the left.
+      targetImageData[k_right] = temp;                    // Copy the old left value to the right.
+      // Swap the green level.
+      temp = targetImageData[k_left + 1];                         // Store the left value to temporary memory.
+      targetImageData[k_left + 1] = targetImageData[k_right + 1]; // Copy the right value to the left.
+      targetImageData[k_right + 1] = temp;                        // Copy the old left value to the right.
+      // Swap the red level.
+      temp = targetImageData[k_left + 2];                         // Store the left value to temporary memory.
+      targetImageData[k_left + 2] = targetImageData[k_right + 2]; // Copy the right value to the left.
+      targetImageData[k_right + 2] = temp;                        // Copy the old left value to the right.
+    }
+  }
+}
+
+void flip(unsigned char *targetImageData, Header header)
+{
+  int rowSize;  // Number of bytes in a row.
+  int fillings; // Number of bytes of padding 0's.
+  int i, j, k;  // Loop variable.
+
+  // up and down
+  int k_low;         // Index of the lower-half pixel.
+  int k_up;          // Index of the upper-half pixel.
+  unsigned int temp; // Temporary memory for pixel swapping.
+
+  fillings = (4 - (header.Width * 3) % 4) % 4; // The number of filling bytes at the end of a row.
+  rowSize = header.Width * 3 + fillings;       // The number of bytes in a row.
+
+  //
+  // Turn the image to right.
+  //
+  for (i = 0; i < header.Height / 2; i++)
+  { // Split the image in to the lower half and the upper half.
+    for (j = 0; j < header.Width; j++)
+    {                                                   // Go through all colums.
+      k_low = i * rowSize + j * 3;                      // The index of the swapped pixel on the lower hlaf in column j.
+      k_up = (header.Height - 1 - i) * rowSize + j * 3; // The index of the swapped pixel on the upper hlaf in column j.
+      // Swap the i-th pixel and (Height-1-i)-th pixel in the j-th column.
+      // Swap blue level.
+      temp = targetImageData[k_low];                  // Store the lower-half value to temporary memory.
+      targetImageData[k_low] = targetImageData[k_up]; // Copy the topper-half value to the lower-half.
+      targetImageData[k_up] = temp;                   // Copy the old lower-half value to the topper-half.
+      // Swap the green level.
+      temp = targetImageData[k_low + 1];                      // Store the lower value to temporary memory.
+      targetImageData[k_low + 1] = targetImageData[k_up + 1]; // Copy the topper-half value to the lower-half.
+      targetImageData[k_up + 1] = temp;                       // Copy the old lower-half value to the topper-half.
+      // Swap the red level.
+      temp = targetImageData[k_low + 2];                      // Store the lower value to temporary memory.
+      targetImageData[k_low + 2] = targetImageData[k_up + 2]; // Copy the topper-half value to the lower-half.
+      targetImageData[k_up + 2] = temp;                       // Copy the old lower-half value to the topper-half.
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
   FILE *fptr;               // File pointer
@@ -34,6 +152,22 @@ int main(int argc, char *argv[])
   int rowSize;              // Number of bytes in a row.
   int fillings;             // Number of bytes of padding 0's.
   int i, j, k;              // Loop variable.
+
+  // right and left
+  int k_left;  // Index of the left-half pixel.
+  int k_right; // Index of the right-half pixel.
+
+  // up and down
+  int k_low;         // Index of the lower-half pixel.
+  int k_up;          // Index of the upper-half pixel.
+  unsigned int temp; // Temporary memory for pixel swapping.
+
+  // original
+  int originalSize;
+  int originalRowSize;
+  int originalFillings;
+  int originalWidth, originalHeight, originalImageSize;
+  unsigned char *tempImageData; // Created for fliping the image.
 
   unsigned char *smallImageData;
   int smallRowSize;
@@ -106,8 +240,12 @@ int main(int argc, char *argv[])
   imageData = (unsigned char *)malloc(header.ImageSize); // Allocate memory space for image pixel data.
   fread(imageData, header.ImageSize, 1, fptr);           // Read image pixel data from the image file.
 
+  // merge init
+  tempImageData = (unsigned char *)malloc(originalImageSize);
+
   fclose(fptr); // Close the input file.
 
+  // Shrink
   smallWidth = header.Width / 2;
   smallHeight = header.Height / 2;
   smallFillings = (4 - (smallWidth * 3) % 4) % 4;
@@ -117,23 +255,102 @@ int main(int argc, char *argv[])
 
   fillings = (4 - (header.Width * 3) % 4) % 4; // The number of filling bytes at the end of a row.
   rowSize = header.Width * 3 + fillings;       // The number of bytes in a row.
-  for (i = 0; i < header.Height; i += 2)
-  { // Go through all rows.
-    for (j = 0; j < header.Width; j += 2)
-    {                          // Go through all pixels in row i.
-      k = i * rowSize + j * 3; // The starting index in imageData of the pixel to be transformed.
-      m = (i / 2) * smallRowSize + (j / 2) * 3;
 
-      smallImageData[m] = imageData[k];         // value of B
-      smallImageData[m + 1] = imageData[k + 1]; // Copy to value of G.
-      smallImageData[m + 2] = imageData[k + 2]; // Copy to value of R.
-    }
-  }
+  // original data
+  originalFillings = fillings;
+  originalRowSize = rowSize;
+  originalWidth = header.Width;
+  originalHeight = header.Height;
+  originalImageSize = header.ImageSize;
+  originalSize = header.Size;
 
+  //
+  // Shrink the size of original image and store it into new image.
+  //
+  shrink(smallImageData, imageData, header);
+
+  fillings = smallFillings;
+  rowSize = smallRowSize;
   header.Width = smallWidth;
   header.Height = smallHeight;
   header.ImageSize = smallImageSize;
   header.Size = header.ImageSize + header.OffsetBits;
+
+  // //
+  // // Turn the image to right.
+  // //
+  //
+  // up and down
+  //
+  flip(smallImageData, header);
+
+  // merge (a)
+
+  for (i = 0; i < smallHeight; i++)
+  { // Go through all rows.
+    for (j = 0; j < smallWidth; j++)
+    {                                  // Go through all pixels in row i.
+      k = i * originalRowSize + j * 3; // The starting index in imageData of the pixel to be transformed.
+      m = i * smallRowSize + j * 3;
+
+      tempImageData[k] = smallImageData[m];         // value of B
+      tempImageData[k + 1] = smallImageData[m + 1]; // Copy to value of G.
+      tempImageData[k + 2] = smallImageData[m + 2]; // Copy to value of R.
+    }
+  }
+
+  mirror(smallImageData, header);
+
+  // merge (a)
+  for (i = 0; i < smallHeight; i++)
+  { // Go through all rows.
+    for (j = 0; j < smallWidth; j++)
+    {                                                 // Go through all pixels in row i.
+      k = i * originalRowSize + smallRowSize + j * 3; // The starting index in imageData of the pixel to be transformed.
+      m = i * smallRowSize + j * 3;
+
+      tempImageData[k] = smallImageData[m];         // value of B
+      tempImageData[k + 1] = smallImageData[m + 1]; // Copy to value of G.
+      tempImageData[k + 2] = smallImageData[m + 2]; // Copy to value of R.
+    }
+  }
+
+  flip(smallImageData, header);
+  // // merge (a)
+  for (i = 0; i < smallHeight; i++)
+  { // Go through all rows.
+    for (j = 0; j < smallWidth; j++)
+    {                                                                        // Go through all pixels in row i.
+      k = (i * originalRowSize + smallRowSize + j * 3) + 2 * smallImageSize; // The starting index in imageData of the pixel to be transformed.
+      m = i * smallRowSize + j * 3;
+
+      tempImageData[k] = smallImageData[m];         // value of B
+      tempImageData[k + 1] = smallImageData[m + 1]; // Copy to value of G.
+      tempImageData[k + 2] = smallImageData[m + 2]; // Copy to value of R.
+    }
+  }
+
+  // 左上
+  mirror(smallImageData, header);
+  // // merge (a)
+  for (i = 0; i < smallHeight; i++)
+  { // Go through all rows.
+    for (j = 0; j < smallWidth; j++)
+    {                                                                        // Go through all pixels in row i.
+      k = (i * originalRowSize + j * 3) + smallImageSize * 2; // The starting index in imageData of the pixel to be transformed.
+      m = i * smallRowSize + j * 3;
+
+      tempImageData[k] = smallImageData[m];         // value of B
+      tempImageData[k + 1] = smallImageData[m + 1]; // Copy to value of G.
+      tempImageData[k + 2] = smallImageData[m + 2]; // Copy to value of R.
+    }
+  }
+
+  // reset
+  header.Width = originalWidth;
+  header.Height = originalHeight;
+  header.ImageSize = originalImageSize;
+  header.Size = originalSize;
 
   // If the input command has three strings, use the 3rd one as the output file name;
   // otherwise, use the default file name "gray_level.bmp".
@@ -148,13 +365,15 @@ int main(int argc, char *argv[])
     printf("\nThe gray-level image is \"gray_level.bmp\".\n\n"); // Print the output file name message.
   }
   // Output the gray-level image file.
-  fwrite(&header.Type, 1, 2, fptr);            // Two fixed characters, "BM".
-  fwrite(&header.Size, 4, 1, fptr);            // File size in bytes.
-  fwrite(&header.Reserved, 1, 4, fptr);        // Reserved field.
-  fwrite(&header.OffsetBits, 4, 1, fptr);      // Offset.
-  fwrite(&header.InfoSize, 4, 1, fptr);        // Information size in byte.
-  fwrite(&header.Width, 4, 1, fptr);           // Image width in byte.
-  fwrite(&header.Height, 4, 1, fptr);          // Image height in byte.
+  fwrite(&header.Type, 1, 2, fptr);       // Two fixed characters, "BM".
+  fwrite(&header.Size, 4, 1, fptr);       // File size in bytes.
+  fwrite(&header.Reserved, 1, 4, fptr);   // Reserved field.
+  fwrite(&header.OffsetBits, 4, 1, fptr); // Offset.
+  fwrite(&header.InfoSize, 4, 1, fptr);   // Information size in byte.
+
+  fwrite(&header.Width, 4, 1, fptr);  // Image width in byte.
+  fwrite(&header.Height, 4, 1, fptr); // Image height in byte.
+
   fwrite(&header.Planes, 2, 1, fptr);          // Number of image planes in the image, must be 1.
   fwrite(&header.BitPerPixel, 2, 1, fptr);     // Number of bits used to represent the data for each pixel.
   fwrite(&header.Compression, 4, 1, fptr);     // Value indicating what type of compression.
@@ -165,7 +384,7 @@ int main(int argc, char *argv[])
   fwrite(&header.ImportantColors, 4, 1, fptr); // Number of colors that are considered important when rendering the image.
 
   fwrite(palette, 1, header.OffsetBits - header.InfoSize - 14, fptr); // Write the palette.
-  fwrite(smallImageData, 1, header.ImageSize, fptr);                       // Write the image pixel data.
+  fwrite(tempImageData, 1, header.ImageSize, fptr);                   // Write the image pixel data.
   fclose(fptr);                                                       // Close the output file.
 
   // Print the gray-level image file head.
@@ -195,5 +414,7 @@ int main(int argc, char *argv[])
   free(imageData); // Release memory space of image pixel data.
 
   free(smallImageData);
+  free(tempImageData);
+
   return 0;
 }
